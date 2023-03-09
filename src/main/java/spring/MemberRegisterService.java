3dad4d33c097e6299ce3b8b310ffc87e8a36aed9
sunfkkc <rlcks77@nate.com>
2023-03-09 14:59:35 +0900

    feat: MemberRegisterService 클래스 생성

---
+++ src/main/java/spring/MemberRegisterService.java
@@ -0,0 +1,26 @@
+package spring;
+
+import java.time.LocalDateTime;
+
+public class MemberRegisterService {
+    private MemberDao memberDao;
+
+    public MemberRegisterService(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public Long regist( RegisterRequest request){
+
+        Member member = memberDao.selectByEmail(request.getEmail());
+
+        if( member != null){
+            throw new DuplicateMemberException("아이디 중복"+request.getEmail());
+        }
+
+        Member newMember = new Member(request.getEmail(), request.getPassword(), request.getName(), LocalDateTime.now());
+
+        memberDao.insert(newMember);
+
+        return newMember.getId();
+    }
+}
