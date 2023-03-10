da623b15446fce2f823b1a5759e8daad1ea715d9
sunfkkc <rlcks77@nate.com>
2023-03-10 16:25:05 +0900

    feat: 프록시 클래스를 스프링 설정 클래스에 등록 후 테스팅

---
+++ src/main/java/config/AOPConf.java
@@ -0,0 +1,23 @@
+package config;
+
+import AOPSample.Calculator;
+import AOPSample.ExeTimeAspect;
+import AOPSample.RecCalculator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AOPConf {
+
+    @Bean
+    public ExeTimeAspect exeTimeAspect(){
+        return new ExeTimeAspect();
+    }
+
+    @Bean
+    public Calculator calculator(){
+        return new RecCalculator();
+    }
+}
---
+++ src/main/java/main/MainForAOP.java
@@ -0,0 +1,21 @@
+package main;
+
+import AOPSample.Calculator;
+import config.AOPConf;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainForAOP {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AOPConf.class);
+
+        Calculator calculator = context.getBean("calculator",Calculator.class);
+
+        long result = calculator.factorial(10);
+        System.out.println("10팩토리얼 :"+result);
+        System.out.println(calculator.getClass().getName());
+
+        context.close();
+    }
+}
