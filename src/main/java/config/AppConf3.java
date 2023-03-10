be5b6a7deb28694202ae540b419ed04a81f08d5d
sunfkkc <rlcks77@nate.com>
2023-03-10 14:26:30 +0900

    feat: Filter를 이용한 컴포넌트 필터링

--- src/main/java/config/AppConf3.java
+++ src/main/java/config/AppConf3.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import spring.AutowiredSample;
 import spring.SomeInjection;
 
 @Configuration
-@ComponentScan(basePackages = {"spring"})
+@ComponentScan(basePackages = {"spring"}, excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "spring\\..*Sample"))
 public class AppConf3 {
 
     @Bean
