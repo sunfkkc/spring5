5ca8e5642fa739d4c250cb6991f99b57370971ba
sunfkkc <rlcks77@nate.com>
2023-03-13 15:52:16 +0900

    feat: MemberDao

---
+++ src/main/java/DBConnection/MemberDao.java
@@ -0,0 +1,19 @@
+package DBConnection;
+
+import java.util.Collection;
+
+public class MemberDao {
+
+    public Member selectByEmail(String email){
+
+        return null;
+    }
+
+    public void insert(Member member){}
+
+    public void update(Member member){}
+
+    public Collection<Member> selectAll(){
+        return null;
+    }
+}
