0f4ed33d73deb6d901df9236783d938d8891aec6
sunfkkc <rlcks77@nate.com>
2023-03-15 11:17:32 +0900

    refactor: jdbcTemplate 멤버변수, 생성자 추가

--- src/main/java/DBConnection/MemberDao.java
+++ src/main/java/DBConnection/MemberDao.java
@@ -1,8 +1,17 @@
 package DBConnection;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 import java.util.Collection;
 
 public class MemberDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MemberDao(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     public Member selectByEmail(String email){
 
