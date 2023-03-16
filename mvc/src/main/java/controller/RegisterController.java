55db859cafb6d262c943f1121bb0e7178542d564
sunfkkc <rlcks77@nate.com>
2023-03-16 19:46:08 +0900

    update: register/step3 요청 매핑 추가

--- mvc/src/main/java/controller/RegisterController.java
+++ mvc/src/main/java/controller/RegisterController.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import spring.DuplicateMemberException;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
 
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
+
+    private MemberRegisterService memberRegisterService;
+
+    public void setMemberRegisterService(MemberRegisterService memberRegisterService) {
+        this.memberRegisterService = memberRegisterService;
+    }
 
     @RequestMapping("/step1")
     public String handleStep1(){
@@ -18,9 +27,19 @@ public class RegisterController {
     public String handleStep2(
             @RequestParam(value = "agree", defaultValue = "false") Boolean agree
     ){
-        if( !agree) return "redirect:/register/step1";
+        if( !agree) return "redirect:step1";
 
         return "register/step2";
+    }
+
+    @PostMapping("/step3")
+    public String handleStep3(RegisterRequest regReq){
+        try{
+            memberRegisterService.regist(regReq);
+            return "register/step3";
+        }catch (DuplicateMemberException ex){
+            return "redirect:step2";
+        }
     }
 
 
