387a4a274be3cc29dfe57f890a3a9a2dd59384f5
sunfkkc <rlcks77@nate.com>
2023-03-16 17:41:30 +0900

    feat: 회원가입 step1을 위한 controller와 view 추가

--- mvc/src/main/java/config/ControllerConfig.java
+++ mvc/src/main/java/config/ControllerConfig.java
@@ -1,5 +1,6 @@
 package config;
 
+import controller.RegisterController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,11 @@ import chap09.HelloController;
 
 @Configuration
 public class ControllerConfig {
+
+    @Bean
+    public RegisterController registerController(){
+        return new RegisterController();
+    }
 
     @Bean
     public HelloController helloController() {
---
+++ mvc/src/main/java/controller/RegisterController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+    @RequestMapping("/step1")
+    public String handleStep1(){
+        return "register/step1";
+    }
+
+
+}
