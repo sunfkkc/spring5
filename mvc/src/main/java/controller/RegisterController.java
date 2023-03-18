4bcf889133603cc29fb7c59869359374bd8b3703
sunfkkc <rlcks77@nate.com>
2023-03-19 01:11:55 +0900

    update: RegisterController에 RegisterRequestValidator 추가

--- mvc/src/main/java/controller/RegisterController.java
+++ mvc/src/main/java/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,10 +40,21 @@ public class RegisterController {
     }
 
     @PostMapping("/step3")
-    public String handleStep3(RegisterRequest regReq){
+    public String handleStep3(RegisterRequest regReq, Errors errors){
+
+        //TODO: 이메일 항목이 비어있을 때 step2로 라우팅 되지 않고 step3로 라우팅 됨
+        new RegisterRequestValidator().validate(regReq,errors);
+        if( errors.hasErrors()) {
+            return "register/step2";
+        }
+
+
         try{
             memberRegisterService.regist(regReq);
+            System.out.println("go step3");
             return "register/step3";
+
+
         }catch (DuplicateMemberException ex){
             return "redirect:step2";
         }
