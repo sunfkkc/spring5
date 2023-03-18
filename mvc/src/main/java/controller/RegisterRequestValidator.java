ad27815846dcc2e46913567187cca67b4f2c9d5e
sunfkkc <rlcks77@nate.com>
2023-03-19 01:09:04 +0900

    feat: 회원가입 폼을 검증하는 RegisterRequestValidator 클래스 추가

---
+++ mvc/src/main/java/controller/RegisterRequestValidator.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import spring.RegisterRequest;
+
+public class RegisterRequestValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return RegisterRequest.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        RegisterRequest request = (RegisterRequest) target;
+        if( request.getEmail() == null || request.getEmail().trim().isEmpty()){
+            errors.rejectValue("email","필수사항");
+        }
+    }
+}
