e12d4d6a5cbc0343e242c3007f7b913809cd55ac
sunfkkc <rlcks77@nate.com>
2023-03-20 00:54:50 +0900

    chore: 메시지 프로퍼티를 사용할 수 있게 설정

--- mvc/src/main/java/config/MvcConfig.java
+++ mvc/src/main/java/config/MvcConfig.java
@@ -1,7 +1,10 @@
 package config;
 
 import controller.RegisterRequestValidator;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -20,6 +23,14 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/view/", ".jsp");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setBasenames("message.label");
+        ms.setDefaultEncoding("UTF-8");
+        return ms;
     }
 
 }
