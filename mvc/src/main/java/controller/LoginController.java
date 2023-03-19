3e592570d88e78c74b62f2acc71dd6bd41e7f4b5
sunfkkc <rlcks77@nate.com>
2023-03-20 00:45:04 +0900

    feat: LoginController

---
+++ mvc/src/main/java/controller/LoginController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.AuthInfo;
+import spring.AuthService;
+import spring.WrongIdPasswordException;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    private AuthService authService;
+
+    public void setAuthService(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping
+    public String form(LoginCommand loginCommand){
+        return "login/loginForm";
+    }
+
+    @PostMapping
+    public String submit(@Valid LoginCommand loginCommand, Errors errors){
+
+        if(errors.hasErrors()){
+
+            return "login/loginForm";
+        }
+
+        try{
+            AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
+
+            return "login/loginSuccess";
+        }
+        catch (WrongIdPasswordException e){
+            errors.reject("idPasswordNotMatching");
+            return "login/loginForm";
+        }
+
+    }
+}
