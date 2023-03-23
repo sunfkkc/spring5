faee87984f5e095dfef8cb0730b42bc3a765bbca
sunfkkc <rlcks77@nate.com>
2023-03-24 00:57:34 +0900

    refactor: authService에 Autowired 애너테이션 추가

--- mvc/src/main/java/controller/LoginController.java
+++ mvc/src/main/java/controller/LoginController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 @RequestMapping("/login")
 public class LoginController {
 
+    @Autowired
     private AuthService authService;
 
     public void setAuthService(AuthService authService) {
