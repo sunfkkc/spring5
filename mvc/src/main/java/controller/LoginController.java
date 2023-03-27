b3e8aaf82581260d543d17a0d44afc96fe17af44
sunfkkc <rlcks77@nate.com>
2023-03-27 17:48:03 +0900

    update: 로그인 폼 제출시 이메일을 기억하면 쿠키에 이메일 저장

--- mvc/src/main/java/controller/LoginController.java
+++ mvc/src/main/java/controller/LoginController.java
@@ -3,6 +3,7 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import spring.AuthInfo;
 import spring.AuthService;
 import spring.WrongIdPasswordException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
@@ -25,12 +28,12 @@ public class LoginController {
     }
 
     @GetMapping
-    public String form(LoginCommand loginCommand){
+    public String form(LoginCommand loginCommand ){
         return "login/loginForm";
     }
 
     @PostMapping
-    public String submit(@Valid LoginCommand loginCommand, Errors errors, HttpSession session){
+    public String submit(@Valid LoginCommand loginCommand, Errors errors, HttpSession session, HttpServletResponse response){
 
         if(errors.hasErrors()){
 
@@ -41,6 +44,16 @@ public class LoginController {
             AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
 
             session.setAttribute("authInfo",authInfo);
+
+            Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+            rememberCookie.setPath("/");
+
+            if(loginCommand.isRememberEmail()){
+                rememberCookie.setMaxAge(60*60*24*30);
+            }else{
+                rememberCookie.setMaxAge(0);
+            }
+            response.addCookie(rememberCookie);
 
             return "login/loginSuccess";
         }
