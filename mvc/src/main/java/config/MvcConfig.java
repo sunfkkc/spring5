a0aa71908f5f21e3d0becb81aab0b8621eb6c148
sunfkkc <rlcks77@nate.com>
2023-03-28 14:21:10 +0900

    update: LocalDateTime 형식 데이터를 응답으로 보낼때 "yyyy-MM-dd" 형식으로 포매팅

--- mvc/src/main/java/config/MvcConfig.java
+++ mvc/src/main/java/config/MvcConfig.java
@@ -1,13 +1,23 @@
 package config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import controller.RegisterRequestValidator;
 import interceptor.AuthCheckInterceptor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -41,4 +51,12 @@ public class MvcConfig implements WebMvcConfigurer {
         return new AuthCheckInterceptor();
     }
 
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().serializerByType(LocalDateTime.class,new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd"))).build();
+
+        converters.add(0,new MappingJackson2HttpMessageConverter(objectMapper));
+    }
 }
