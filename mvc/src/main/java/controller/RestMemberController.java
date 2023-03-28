feaaa1f14bbb39e18c3ee4a49ec44bc833d30b52
sunfkkc <rlcks77@nate.com>
2023-03-28 14:51:38 +0900

    update: 멤버를 등록하는 post mapping 추가

--- mvc/src/main/java/controller/RestMemberController.java
+++ mvc/src/main/java/controller/RestMemberController.java
@@ -1,14 +1,11 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import spring.Member;
-import spring.MemberDao;
-import spring.MemberRegisterService;
+import org.springframework.web.bind.annotation.*;
+import spring.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
@@ -32,5 +29,17 @@ public class RestMemberController {
 
         return member;
 
+    }
+
+    @PostMapping("/api/members")
+    public void newMember(@RequestBody @Valid RegisterRequest request, HttpServletResponse response) throws IOException{
+
+        try{
+            Long newMemberId = memberRegisterService.regist(request);
+            response.setHeader("Location","/api/members/"+newMemberId);
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        }catch (DuplicateMemberException e){
+            response.sendError(HttpServletResponse.SC_CONFLICT);
+        }
     }
 }
