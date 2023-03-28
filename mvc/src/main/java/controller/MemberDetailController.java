13d9afbae1498ee16658bbfa0dd6c31710594c6d
sunfkkc <rlcks77@nate.com>
2023-03-28 13:44:53 +0900

    update: MemberDetailController에 ExceptionHandler 추가

--- mvc/src/main/java/controller/MemberDetailController.java
+++ mvc/src/main/java/controller/MemberDetailController.java
@@ -1,8 +1,10 @@
 package controller;
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import spring.Member;
@@ -22,4 +24,15 @@ public class MemberDetailController {
         model.addAttribute("member",member);
         return "member/memberDetail";
     }
+
+    @ExceptionHandler(TypeMismatchException.class)
+    public String handleTypeMismatchException(){
+        return "member/invalidId";
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public String handleNotFoundException(){
+        return "member/noMember";
+    }
+
 }
