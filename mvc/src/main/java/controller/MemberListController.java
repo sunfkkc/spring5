5aacc761b1d4efe859b3f2f45240db1c99942bac
sunfkkc <rlcks77@nate.com>
2023-03-28 11:50:53 +0900

    update: 에러가 발생하면 memberList.jsp 리턴

--- mvc/src/main/java/controller/MemberListController.java
+++ mvc/src/main/java/controller/MemberListController.java
@@ -3,6 +3,7 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.Member;
@@ -17,7 +18,9 @@ public class MemberListController {
     private MemberDao memberDao;
 
     @RequestMapping("/members")
-    public String list(@ModelAttribute("cmd") ListCommand listCommand, Model model){
+    public String list(@ModelAttribute("cmd") ListCommand listCommand, Errors errors, Model model){
+
+        if(errors.hasErrors()) return "member/memberList";
 
         if( listCommand.getFrom() != null && listCommand.getTo() != null){
             List<Member> members = memberDao.selectByRegdate(listCommand.getFrom(), listCommand.getTo());
