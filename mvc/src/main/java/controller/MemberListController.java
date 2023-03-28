ab9e67c46c2c085ed3132505bdbd4fcebe426819
sunfkkc <rlcks77@nate.com>
2023-03-28 11:20:16 +0900

    feat: MemberListController

---
+++ mvc/src/main/java/controller/MemberListController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.Member;
+import spring.MemberDao;
+
+import java.util.List;
+
+@Controller
+public class MemberListController {
+
+    @Autowired
+    private MemberDao memberDao;
+
+    @RequestMapping("/members")
+    public String list(@ModelAttribute("cmd") ListCommand listCommand, Model model){
+
+        if( listCommand.getFrom() != null && listCommand.getTo() != null){
+            List<Member> members = memberDao.selectByRegdate(listCommand.getFrom(), listCommand.getTo());
+            model.addAttribute("members",members);
+        }
+        return "member/memberList";
+    }
+}
