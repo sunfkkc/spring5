b925b3e422d347c6bfc526417cc6f63d3e992c46
sunfkkc <rlcks77@nate.com>
2023-03-30 17:08:53 +0900

    test: /hello/dto getmethod test

--- spring-boot/src/test/java/springboot/web/HelloControllerTest.java
+++ spring-boot/src/test/java/springboot/web/HelloControllerTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import springboot.web.dto.HelloResponseDto;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -27,5 +29,14 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+
+
+        mvc.perform(get("/hello/dto").param("name","kkc").param("amount",String.valueOf(100))).andExpect(status().isOk()).andExpect(jsonPath("$.name",is("kkc"))).andExpect(jsonPath("$.amount",is(100)));
+
+
     }
 }
