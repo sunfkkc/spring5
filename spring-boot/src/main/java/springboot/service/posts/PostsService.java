8f02b1c193e393ec5acad19759527dc4d60d7620
sunfkkc <rlcks77@nate.com>
2023-04-02 23:48:32 +0900

    update: PostsService에 update 메서드 추가

--- spring-boot/src/main/java/springboot/service/posts/PostsService.java
+++ spring-boot/src/main/java/springboot/service/posts/PostsService.java
@@ -7,6 +7,7 @@ import springboot.domain.posts.Posts;
 import springboot.domain.posts.PostsRepository;
 import springboot.web.dto.PostsResponseDto;
 import springboot.web.dto.PostsSaveRequestDto;
+import springboot.web.dto.PostsUpdateRequestDto;
 
 @RequiredArgsConstructor
 @Service
@@ -21,5 +22,14 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
     }
 }
