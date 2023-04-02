ab0e72a9990d483970dd2da030bde2cf5d3916a5
sunfkkc <rlcks77@nate.com>
2023-04-02 23:26:36 +0900

    feat: PostsResponseDto

---
+++ spring-boot/src/main/java/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package springboot.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import springboot.domain.posts.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
