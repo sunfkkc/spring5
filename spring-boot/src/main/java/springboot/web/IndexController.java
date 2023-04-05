950cf854dafee968ef516d3209f832a6099a181a
sunfkkc <rlcks77@nate.com>
2023-04-05 16:09:57 +0900

    fix: 게시글 수정 url에 /가 빠진 것을 수정

--- spring-boot/src/main/java/springboot/web/IndexController.java
+++ spring-boot/src/main/java/springboot/web/IndexController.java
@@ -27,7 +27,7 @@ public class IndexController {
     }
 
 
-    @GetMapping("/posts/update{id}")
+    @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post",dto);
