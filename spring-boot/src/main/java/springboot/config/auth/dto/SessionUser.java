386112b73d0ae8f39fd0359732f72209b1771a42
sunfkkc <rlcks77@nate.com>
2023-04-06 00:39:14 +0900

    feat: 직렬화 기능을 가진 세션 Dto 생성

---
+++ spring-boot/src/main/java/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package springboot.config.auth.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import springboot.domain.user.User;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
