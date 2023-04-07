51e04c82800740c6373cd3494b6144a4ba52c982
sunfkkc <rlcks77@nate.com>
2023-04-07 18:13:46 +0900

    refactor: Httpsession 의존성 제거

--- spring-boot/src/main/java/springboot/web/IndexController.java
+++ spring-boot/src/main/java/springboot/web/IndexController.java
@@ -17,7 +17,6 @@ import springboot.web.dto.PostsResponseDto;
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user){
