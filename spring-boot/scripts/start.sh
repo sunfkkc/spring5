aaba8e68855fd6331bff1f96e38e27ed0ad09a1e
sunfkkc <rlcks77@nate.com>
2023-04-12 17:56:03 +0900

    fix: 실행중이던 프로그램을 kill 해버리던 것을 수정

--- spring-boot/scripts/start.sh
+++ spring-boot/scripts/start.sh
@@ -11,19 +11,6 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl spring-boot | grep java | awk '{print $1}')
-
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
 
 echo "> 새 어플리케이션 배포"
 
